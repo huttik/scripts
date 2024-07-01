178984a4cfdf8bb7d1d597c2c846b330142ebe7d
huttik <serg.kirill.eev@icloud.com>
2024-07-01 18:59:27 +0300

    Update tapswap-web.user.js

--- TapSwap/tapswap-web.user.js
+++ TapSwap/tapswap-web.user.js
@@ -3,13 +3,13 @@
 // @namespace    http://tampermonkey.net/
 // @version      1.3
 // @description  Running TapSwap in a browser with random iOS User-Agent
-// @author       mudachyo
+// @author       huttik
 // @match        *://*.tapswap.*/*
 // @grant        none
 // @icon         https://www.softportal.com/en/scr/1089/icons/icon_src.png
-// @downloadURL  https://github.com/mudachyo/TapSwap/raw/main/tapswap-web.user.js
-// @updateURL    https://github.com/mudachyo/TapSwap/raw/main/tapswap-web.user.js
-// @homepage     https://github.com/mudachyo/TapSwap
+// @downloadURL  https://github.com/huttik/scripts/edit/main/TapSwap/tapswap-web.user.js
+// @updateURL    https://github.com/huttik/scripts/edit/main/TapSwap/tapswap-web.user.js
+// @homepage     https://github.com/huttik/scripts/edit/main/TapSwap
 // ==/UserScript==
 (function() {
     'use strict';
